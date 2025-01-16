8e41292b6ea2fac039c9c909033032af2f88acf6
Will Lynas <43895423+will-lynas@users.noreply.github.com>
2025-01-16 14:26:31 +0000

    Add counter example

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,38 @@
-fn main() {
-    println!("Hello from the chess engine!");
+use iced::{
+    widget::{button, column, text, Column},
+    Alignment::Center,
+};
+
+fn main() -> iced::Result {
+    iced::run("Counter!", Counter::update, Counter::view)
+}
+
+#[derive(Default)]
+struct Counter {
+    value: i64,
+}
+
+impl Counter {
+    fn update(&mut self, message: Message) {
+        match message {
+            Message::Increment => self.value += 1,
+            Message::Decrement => self.value -= 1,
+        }
+    }
+
+    fn view(&self) -> Column<Message> {
+        column![
+            button("Increment").on_press(Message::Increment),
+            button("Decrement").on_press(Message::Decrement),
+            text(self.value).size(69),
+        ]
+        .padding(16)
+        .align_x(Center)
+    }
+}
+
+#[derive(Clone, Debug)]
+enum Message {
+    Increment,
+    Decrement,
 }
