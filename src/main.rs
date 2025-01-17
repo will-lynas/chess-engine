e7c1e42a33f20cde6c0f6eda6f559c5c844d4028
Will Lynas <43895423+will-lynas@users.noreply.github.com>
2025-01-17 16:29:00 +0000

    Draw square

--- src/main.rs
+++ src/main.rs
@@ -1,38 +1,63 @@
 use iced::{
-    widget::{button, column, text, Column},
+    mouse,
+    widget::{
+        canvas::{self, Geometry},
+        column, Canvas,
+    },
     Alignment::Center,
+    Length::Fill,
+    Point, Rectangle, Renderer, Size, Theme,
 };
 
 fn main() -> iced::Result {
-    iced::run("Counter!", Counter::update, Counter::view)
+    iced::application("Chess Game", App::update, App::view).run()
 }
 
 #[derive(Default)]
-struct Counter {
-    value: i64,
+struct App {
+    board: Board,
 }
 
-impl Counter {
-    fn update(&mut self, message: Message) {
-        match message {
-            Message::Increment => self.value += 1,
-            Message::Decrement => self.value -= 1,
-        }
-    }
+impl App {
+    fn update(&mut self, _message: Message) {}
 
-    fn view(&self) -> Column<Message> {
-        column![
-            button("Increment").on_press(Message::Increment),
-            text(self.value).size(69),
-            button("Decrement").on_press(Message::Decrement),
-        ]
-        .padding(16)
-        .align_x(Center)
+    fn view(&self) -> iced::Element<'_, Message> {
+        column![Canvas::new(&self.board).width(Fill).height(Fill),]
+            .align_x(Center)
+            .into()
     }
 }
 
-#[derive(Clone, Debug)]
-enum Message {
-    Increment,
-    Decrement,
+#[derive(Debug)]
+struct Message;
+
+#[derive(Default)]
+struct Board {
+    cache: canvas::Cache,
+}
+
+impl canvas::Program<Message> for Board {
+    type State = ();
+
+    fn draw(
+        &self,
+        _state: &Self::State,
+        renderer: &Renderer,
+        _theme: &Theme,
+        bounds: Rectangle,
+        _cursor: mouse::Cursor,
+    ) -> Vec<Geometry> {
+        let geom = self.cache.draw(renderer, bounds.size(), |frame| {
+            let square_size = bounds.width.min(bounds.height) * 0.5;
+            let top_left = Point {
+                x: (bounds.width - square_size) / 2.0,
+                y: (bounds.height - square_size) / 2.0,
+            };
+            let rectangle = canvas::Path::rectangle(top_left, Size::new(square_size, square_size));
+
+            frame.fill(&rectangle, iced::Color::BLACK);
+        });
+
+        vec![geom]
+    }
 }
