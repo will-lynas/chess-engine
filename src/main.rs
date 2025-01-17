79a3232cef760ed52f8f7c84416afcd29fc19a6f
Will Lynas <43895423+will-lynas@users.noreply.github.com>
2025-01-17 16:33:41 +0000

    Qualify less, import more

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,18 @@
 use iced::{
-    mouse,
+    application,
+    mouse::Cursor,
     widget::{
-        canvas::{self, Geometry},
+        canvas::{Cache, Geometry, Path, Program},
         column, Canvas,
     },
     Alignment::Center,
+    Color, Element,
     Length::Fill,
     Point, Rectangle, Renderer, Size, Theme,
 };
 
 fn main() -> iced::Result {
-    iced::application("Chess Game", App::update, App::view).run()
+    application("Chess Game", App::update, App::view).run()
 }
 
 #[derive(Default)]
@@ -21,7 +23,7 @@ struct App {
 impl App {
     fn update(&mut self, _message: Message) {}
 
-    fn view(&self) -> iced::Element<'_, Message> {
+    fn view(&self) -> Element<'_, Message> {
         column![Canvas::new(&self.board).width(Fill).height(Fill),]
             .align_x(Center)
             .into()
@@ -33,10 +35,10 @@ struct Message;
 
 #[derive(Default)]
 struct Board {
-    cache: canvas::Cache,
+    cache: Cache,
 }
 
-impl canvas::Program<Message> for Board {
+impl Program<Message> for Board {
     type State = ();
 
     fn draw(
@@ -45,7 +47,7 @@ impl canvas::Program<Message> for Board {
         renderer: &Renderer,
         _theme: &Theme,
         bounds: Rectangle,
-        _cursor: mouse::Cursor,
+        _cursor: Cursor,
     ) -> Vec<Geometry> {
         let geom = self.cache.draw(renderer, bounds.size(), |frame| {
             let square_size = bounds.width.min(bounds.height) * 0.5;
@@ -53,9 +55,9 @@ impl canvas::Program<Message> for Board {
                 x: (bounds.width - square_size) / 2.0,
                 y: (bounds.height - square_size) / 2.0,
             };
-            let rectangle = canvas::Path::rectangle(top_left, Size::new(square_size, square_size));
+            let rectangle = Path::rectangle(top_left, Size::new(square_size, square_size));
 
-            frame.fill(&rectangle, iced::Color::BLACK);
+            frame.fill(&rectangle, Color::BLACK);
         });
 
         vec![geom]
